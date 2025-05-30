d17ed802612428d3168e535a4a58efbab55086c7
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-30 18:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ function volume_sphere() {
    let r = parseFloat(document.getElementById("radius").value);
    let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
    document.getElementById("volume").value = volume.toFixed(4); 
-} 
+}
+	window.onload = function () {
+    // This is where preventDefault() is correctly used
+    document.getElementById("MyForm").addEventListener("submit", function(e) {
+        e.preventDefault(); // <-- It's already here!
+        volume_sphere();
+    });
+};function volume_sphere() {
+			let r = parseFloat(document.getElementById("radius").value);
+			let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+			document.getElementById("volume").value = volume.toFixed(4); 
+		}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+		window.onload = function () {
+			document.getElementById("MyForm").addEventListener("submit", function(e) {
+				e.preventDefault();
+				volume_sphere();
+	});
+};
+
