57f46235b3fdea05e0079308afe2bf87dd98eda6
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-05-30 18:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function volume_sphere() {
-    //Write your code here
-  
+   let r = parseFloat(document.getElementById("radius").value);
+   let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+   document.getElementById("volume").value = volume.toFixed(4); 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
